22c8f3e4cd5ac3b09257f0558e26d6394b9675ec
natalia chetelat <nataliachetelat@gmail.com>
2018-03-15 13:34:22 -0400

    update frontend component generator

--- lib/generators/component_generator.rb
+++ lib/generators/component_generator.rb
@@ -1,24 +1,29 @@
 class ComponentGenerator < Rails::Generators::Base
   argument :component_name, required: true, desc: "Component name, e.g: message"
 
-  def create_view_file
-    create_file "#{component_path}/_#{component_name}.html.erb"
-  end
+  def create_vue_file
+    create_file "#{component_path}/#{component_name}.vue" do
+      <<-FILE
+      <template>
+        div.#{component_name}
+      </template>
 
-  def create_css_file
-    create_file "#{component_path}/#{component_name}.css"
-  end
+      <script>
+      export default {
+        name: #{component_name.underscore.camelize}
+      }
+      </script>
 
-  def create_js_file
-    create_file "#{component_path}/#{component_name}.js" do
-      "import \"./#{component_name}.css\"\n"
+      <style scoped>
+      </style>
+      FILE
     end
   end
 
   protected
 
     def component_path
-      "frontend/components/#{component_name}"
+      "frontend/components"
     end
 
 end
