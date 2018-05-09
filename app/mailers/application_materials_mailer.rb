80703e7fcd9c325140e1f6a39160e88ac4e9f2db
natalia chetelat <nataliachetelat@gmail.com>
2018-05-09 11:07:59 -0400

    update email template

--- app/mailers/application_materials_mailer.rb
+++ app/mailers/application_materials_mailer.rb
@@ -5,8 +5,8 @@ class ApplicationMaterialsMailer < ApplicationMailer
     @fields = params[:fields]
     @attachment_path = params[:attachment_path]
     attachments[@attachment_path] = File.read(@attachment_path)
-
-    mail(from: ENV["GMAIL_ADDRESS"], to: ENV["GMAIL_ADDRESS"], subject: "Materials for submitted application")
+    person_name = @application.person.full_name
+    mail(from: ENV["GMAIL_ADDRESS"], to: ENV["GMAIL_ADDRESS"], subject: "Application materials for #{person_name}")
   end
 
 end
