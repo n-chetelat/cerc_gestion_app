5107449a9e0280db42ce8739b723689b792e12ac
natalia chetelat <nataliachetelat@gmail.com>
2018-05-15 13:53:08 -0400

    change default timezone

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,9 @@ module CercGestion
     config.autoload_paths << "#{Rails.root}/*/services"
     config.autoload_paths << "#{Rails.root}/lib/errors"
 
+    config.time_zone = 'Eastern Time (US & Canada)'
+    config.active_record.default_timezone = :local
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
